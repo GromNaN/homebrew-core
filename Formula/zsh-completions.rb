f00c869c7a5877e642068e0c23e1f52758c03947
Jérôme Tamarelle <jerome@tamarelle.net>
2023-04-28 00:15:55 +0200

    Fix instructions to debug ZshCompletion

--- Formula/zsh-completions.rb
+++ Formula/zsh-completions.rb
@@ -38,7 +38,7 @@ class ZshCompletions < Formula
       Additionally, if you receive "zsh compinit: insecure directories" warnings when attempting
       to load these completions, you may need to run this:
 
-        chmod -R go-w '#{HOMEBREW_PREFIX}/share/zsh'
+        chmod -R go-w $(brew --prefix)/share/zsh
     EOS
   end
 
